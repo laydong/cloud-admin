5ffe34cf7ccf0c1a9995173cfc58194fe8d3ada2
dong <68606102@qq.com>
2023-03-14 22:54:53 +0800

    up

--- src/stores/userInfo.ts
+++ src/stores/userInfo.ts
@@ -35,47 +35,12 @@ export const useUserInfo = defineStore('userInfo', {
 			if (this.userInfos.id == 0) {
 				await this.getApiUserInfo();
 			}
-			// 	const userInfos: any = await this.getApiUserInfo();
-			// 	this.userInfos = userInfos;
-			// }
 		},
 		// 模拟接口数据
 		// https://gitee.com/lyt-top/vue-next-admin/issues/I5F1HP
 		async getApiUserInfo() {
 			return new Promise((resolve) => {
 				setTimeout(() => {
-					// // 模拟数据，请求接口时，记得删除多余代码及对应依赖的引入
-					// const userName = Cookies.get('userName');
-					// // 模拟数据
-					// let defaultRoles: Array<string> = [];
-					// let defaultAuthBtnList: Array<string> = [];
-					// // admin 页面权限标识，对应路由 meta.roles，用于控制路由的显示/隐藏
-					// let adminRoles: Array<string> = ['admin'];
-					// // admin 按钮权限标识
-					// let adminAuthBtnList: Array<string> = ['btn.add', 'btn.del', 'btn.edit', 'btn.link'];
-					// // test 页面权限标识，对应路由 meta.roles，用于控制路由的显示/隐藏
-					// let testRoles: Array<string> = ['common'];
-					// // test 按钮权限标识
-					// let testAuthBtnList: Array<string> = ['btn.add', 'btn.link'];
-					// // 不同用户模拟不同的用户权限
-					// if (userName === 'admin') {
-					// 	defaultRoles = adminRoles;
-					// 	defaultAuthBtnList = adminAuthBtnList;
-					// } else {
-					// 	defaultRoles = testRoles;
-					// 	defaultAuthBtnList = testAuthBtnList;
-					// }
-					// // 用户信息模拟数据
-					// const userInfos = {
-					// 	userName: userName,
-					// 	photo:
-					// 		userName === 'admin'
-					// 			? 'https://img2.baidu.com/it/u=1978192862,2048448374&fm=253&fmt=auto&app=138&f=JPEG?w=504&h=500'
-					// 			: 'https://img2.baidu.com/it/u=2370931438,70387529&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
-					// 	time: new Date().getTime(),
-					// 	roles: defaultRoles,
-					// 	authBtnList: defaultAuthBtnList,
-					// };
 					useUserApi().UserInfo().then((res: any)=>{
 						if (res.code == 200 ) {
 							this.userInfos.id = res.data.id
